5c077cc23cd4df5d44f4a5ee03a26af6f3634317
Didier Wojciechowski <dwojciec@redhat.com>
2016-11-07 18:03:30 +0100

    changed sync.sh

--- create/sync.sh
+++ create/sync.sh
@@ -1,7 +1,14 @@
-for repo in \
-rhel-7-server-rpms rhel-7-server-extras-rpms \
-rhel-7-server-ose-3.3-rpms
+#!/bin/bash
+DATE_SYNCHRO="20160711-1"
+REPO_LIST="rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-ose-3.3-rpms"
+REPO_FOLDER="/data/repo/$DATE_SYNCHRO"
+REPO_CONF="$REPO_FOLDER/local.repo"
+mkdir -p $REPO_FOLDER
+rm -f $REPO_CONF
+for repo in $REPO_LIST
 do
-  reposync --gpgcheck -lm --repoid=${repo} --download_path=/orange/repos
-  createrepo -v /orange/repos/${repo} -o /orange/repos/${repo}
+  echo "Sync of $repo"
+  reposync --gpgcheck -lm --repoid=$repo --download_path=$REPO_FOLDER
+  mkdir -p $REPO_FOLDER/$repo
+  createrepo -v $REPO_FOLDER/$repo -o $REPO_FOLDER/$repo
 done
